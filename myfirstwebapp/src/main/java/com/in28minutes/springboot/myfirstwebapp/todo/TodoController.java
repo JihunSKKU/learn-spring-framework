3091a307d3ba41d157b4cf5fc031767e74a4aac7
Jihun <kjh31021@g.skku.edu>
2024-05-28 01:49:49 +0900

    Step 20 - Lets Add a New Todo - Create a new View

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -22,8 +23,20 @@ public class TodoController {
 	public String listAllTodos(ModelMap model) {
 		List<Todo> todos = todoService.findByUsername("jihun");
 		model.addAttribute("todos", todos);
-		
+
 		return "listTodos";
+	}
+
+	//GET, POST
+	@RequestMapping(value="/add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage() {
+		return "todo";
+	}
+	
+	@RequestMapping(value="/add-todo", method = RequestMethod.POST)
+	public String addNewTodo() {
+		
+		return "redirect:list-todos";
 	}
 
 }
